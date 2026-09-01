55f38291baadabe15c46bd39450909693ffb9d48
aefly <288435012+aefly@users.noreply.github.com>
2026-09-01 06:45:22 +0200

    bump(version): 1.3.0 -> 1.3.1

--- xmake.lua
+++ xmake.lua
@@ -6,7 +6,7 @@ set_config("skyrim_vr", false)
 includes("lib/commonlibsse-ng")
 
 set_project("skyzoom")
-set_version("1.3.0")
+set_version("1.3.1")
 set_license("GPL-3.0-or-later")
 set_languages("c++23")
 set_warnings("allextra")
