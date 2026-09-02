7520a026dfa62606d788beda069087c7e53ffdfd
aefly <288435012+aefly@users.noreply.github.com>
2026-09-02 08:45:39 +0200

    style(CompatAPI): Reformat `SkyZoom_GetActiveZoomWeight` signature

--- src/CompatAPI.cpp
+++ src/CompatAPI.cpp
@@ -2,8 +2,9 @@
 
 #include "FOVController.h"
 
-extern "C" __declspec(dllexport) bool SkyZoom_GetActiveZoomWeight(
-    float *a_outWeight, float *a_outTargetFOV) noexcept {
+extern "C" __declspec(dllexport) bool
+SkyZoom_GetActiveZoomWeight(float *a_outWeight,
+                            float *a_outTargetFOV) noexcept {
   return a_outWeight && a_outTargetFOV &&
          FOVController::GetActiveZoomWeight(*a_outWeight, *a_outTargetFOV);
 }
